747ebce87b74f33e2e66be34d777eb28009f0460
dkraus <dkraus@dr-lauterbach.de>
2020-06-23 14:35:16 +0200

    removed unnecessary imports

--- i18nFileComparer/src/components/PropertiesFileHandler.java
+++ i18nFileComparer/src/components/PropertiesFileHandler.java
@@ -1,20 +1,11 @@
 package components;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 
 import components.file.comparing.PropertiesComparer;
 import components.file.comparing.PropertiesComparison;
--- i18nFileComparer/src/components/file/search/PropertiesFileStore.java
+++ i18nFileComparer/src/components/file/search/PropertiesFileStore.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 import utils.PropertiesWithPath;
